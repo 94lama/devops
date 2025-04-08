2cd5f1c810f66e2dd6f1c4db7dd74590decfc279
Riccardo La Malfa <riccardo.lamalfa94@gmail.com>
2025-04-08 12:44:58 +0200

    BREAKING CHANGE: removed chatbot

--- docker/chatbot/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Chat from './chat/Chat';
-
-export default function Page() {
-  return (
-    <main className='justify-content-center align-items-center w-max-[75vh] h-75'>
-      <Chat />
-    </main>
-  );
-}
